179f4ad20829faf27fd78b4d81d5354041991afd
Biro <g.vallarelli@gmail.com>
2020-01-15 00:57:58 +0100

    fixes

---
+++ single.php
@@ -0,0 +1,24 @@
+<?php get_header()?>
+<div id="app" class="inner">
+  <!-- <?php require(__DIR__.'/src/components/navbar.php')?> -->
+  <main class="border-grey">
+
+    <?php while ( have_posts() ) :
+    the_post();
+?>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1><?php the_title(); ?></h1>
+        </div>
+        <div class="col-12"><?php the_content( ); ?></div>
+      </div>
+    </div>
+    <?php
+
+    endwhile; // End of the loop.
+
+    ?>
+  </main>
+</div>
+<?php get_footer()?>
--- src/components/struttura.php
+++ src/components/struttura.php
@@ -1,31 +1,37 @@
 <a name="struttura"></a>
-<div class="page-half page-single border-green py-4 d-flex align-items-center">
-  <div class="container">
-    <div class="row">
-      <?php
-            $post= wp_get_single_post( getenv("HOME_STRUCT") );
-            //setup_postdata( $post );
-            //var_dump($post);
-            ?>
-      <h1 class="text-primary text-uppercase col-md-12"><a href="<?php the_permalink( $post )?>" role="button">Villa
-          Anita</a></h1>
-      <hr />
-      <p class="col-md-12"><?=$post->post_excerpt;?></p>
 
-    </div>
-    <div class="row">
-      <?php
-    $args = array( 'category' =>17 ,'numberposts'=>-1 );
-    $postslist = get_posts( $args );    
-    foreach ($postslist as $post) :  setup_postdata($post); 
-    ?>
-      <div class="col-md-3 col-sm-3">
-        <h5 class="text-uppercase font-weight-bold"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+<div class="page-half page-single border-green ">
+  <div class="container-fluid">
+    <div class="row bgStruct"><img src="<?=get_template_directory_uri(  )?>/src/assets/img/sollievo.jpg" /></div>
+  </div>
+  <div class="d-flex flex-column align-items-center flex-wrapper-100">
+    <div class="container h-50 ">
+      <div class="row">
+        <?php
+              $post= wp_get_single_post( getenv("HOME_STRUCT") );
+              //setup_postdata( $post );
+              //var_dump($post);
+              ?>
+        <h1 class="text-primary text-uppercase col-md-12"><a href="<?php the_permalink( $post )?>" role="button">Villa
+            Anita</a></h1>
+        <hr />
+        <p class="col-md-12"><?=$post->post_excerpt;?></p>
+
       </div>
+      <div class="row">
+        <?php
+      $args = array( 'category' =>17 ,'numberposts'=>-1 );
+      $postslist = get_posts( $args );    
+      foreach ($postslist as $post) :  setup_postdata($post); 
+      ?>
+        <div class="col-md-3 col-sm-3">
+          <h5 class="text-uppercase font-weight-bold"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+        </div>
 
 
-      <?php endforeach; ?>
+        <?php endforeach; ?>
 
+      </div>
     </div>
   </div>
 </div>
