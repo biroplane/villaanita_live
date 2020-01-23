069dfdec67289be0b99227271fde23d8a4177068
Biro <g.vallarelli@gmail.com>
2020-01-23 21:57:38 +0100

    page file added

---
+++ page.php
@@ -0,0 +1,27 @@
+<?php get_header()?>
+<div id="app" class="inner">
+  <?php require(__DIR__.'/src/components/navbar.php')?>
+
+  <main class="border-grey">
+
+    <?php while ( have_posts() ) :
+    the_post();
+?>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="single-title">
+            <h1><?php the_title(); ?></h1>
+          </div>
+        </div>
+        <div class="col-12"><?php the_content( ); ?></div>
+      </div>
+    </div>
+    <?php
+
+    endwhile; // End of the loop.
+
+    ?>
+  </main>
+</div>
+<?php get_footer()?>
