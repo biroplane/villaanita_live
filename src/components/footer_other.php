f4cc3fa13ceb69aae973b8e2077353f35d0be53b
Biro <g.vallarelli@gmail.com>
2020-01-23 23:47:27 +0100

    footer fix

--- src/components/footer_other.php
+++ src/components/footer_other.php
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-md-4"> <img src="<?=get_template_directory_uri(  )?>/src/assets/img/logo.png"
           alt="Villa Anita logo" width="100" class="mx-4"></div>
-      <div class="col-md-8">discleimer</div>
+      <div class="col-md-8"></div>
     </div>
   </div>
 </footer>
