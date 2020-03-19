7293861f1670d15fd180b1f8bf46c6f348b14f5b
Biro <g.vallarelli@gmail.com>
2020-03-19 08:38:43 +0100

    rea e p.iva

--- src/components/footer_home.php
+++ src/components/footer_home.php
@@ -7,9 +7,14 @@
             <img src="<?=get_template_directory_uri(  )?>/src/assets/img/logo.png" alt="Villa Anita logo" width="100"
               class="mx-4">
             <div class="card-body">
-              <p>Strada Provinciale, 2<br>70038 - Terlizzi (Ba)<br>P.Iva 01234567</p>
+              <strong>Villa Anita s.r.l.</strong><br>
+              <p>Strada Provinciale, 2<br>70038 - Terlizzi (Ba)<br>
+                <small>CF / P.Iva 07759950723 - REA 579852<br>Cap Soc
+                  100.000,00 I.V.</small></p>
             </div>
-            <div class="card-footer"><a href="https://www.villaanita.net/informativa-sulla-privacy-e-sui-cookie/">Cookie
+            <div class="card-footer">
+
+              <a href="https://www.villaanita.net/informativa-sulla-privacy-e-sui-cookie/">Cookie
                 Policy & Privacy</a></div>
             <!-- <div class="card-footer">
               <button class="btn btn-outline-primary"><i class="mdi mdi-facebook"></i></button>
@@ -17,28 +22,7 @@
               <button class="btn btn-outline-primary"><i class="mdi mdi-instagram"></i></button>
             </div> -->
           </div>
-          <!-- <div class="container-fluid ">
-            <div class="row">
-              <div class="col-md-12">
-                <div class="form-group"><label for="">Nome</label><input type="text" class="form-control"></div>
-              </div>
-              <div class="col-md-12">
-                <div class="form-group"><label for="">Email</label><input type="text" class="form-control"></div>
-              </div>
-              <div class="col-md-12">
-                <div class="form-group">
-                  <label for="exampleFormControlTextarea1">Messaggio</label>
-                  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                </div>
-              </div>
-              <div class="col-md-12">
-                <div class="form-controlz">
 
-                  <div class="btn btn-outline-primary btn-disabled">Invia</div>
-                </div>
-              </div>
-            </div>
-          </div> -->
           <div class="container-fluid">
             <div class="row">
 
